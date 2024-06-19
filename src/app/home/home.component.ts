02d52e13e0dcfd86a7014a84e9850a9f2c48f4cb
Ferangular <firthunsnpm@gmail.com>
2024-06-19 23:01:54 +0200

    Feat: finalizado primera sección: Introducción angular Signals

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
+import {afterNextRender, Component, computed, effect, EffectRef, inject, Injector, signal} from '@angular/core';
 import {CoursesService} from "../services/courses.service";
 import {Course} from "../models/course.model";
 import {MatTab, MatTabGroup} from "@angular/material/tabs";
@@ -9,6 +9,7 @@ import {MatDialog} from "@angular/material/dialog";
 type Counter = {
   value: number;
 }
+
 @Component({
   selector: 'home',
   standalone: true,
@@ -22,44 +23,82 @@ type Counter = {
 })
 export class HomeComponent {
 
-  #courses = signal<Course[]>([]);
-
-  coursesService = inject(CoursesService);
 
   dialog = inject(MatDialog);
 
 
-counter = signal<Counter>({
-  value: 0
-})
+  counter = signal<Counter>({value: 0})
+  values = signal<number[]>([0]);
 
-increment(){
-this.counter.update(c => ({...c, value: c.value + 1}));
-}
+  tenXCounter = computed(()=>{
+    return this.counter().value * 10
+  });
 
+  hundredXCounter = computed(()=>{
+    return this.tenXCounter() * 100
+  });
 
+  /**
+   * Así que ahora, porque hemos pasado aquí manualmente este
+   * inyector específico que está vinculado al ciclo de vida
+   * de esta instancia de componente.
+   * Cuando el inyector se limpie, también va a activar la limpieza de este efecto.
+   */
+  injector = inject(Injector);
 
-  onCourseDeleted($event: Event) {
-throw new Error('Method not implemented.');
-}
-advancedCourses() {
-throw new Error('Method not implemented.');
-}
-beginnerCourses() {
-throw new Error('Method not implemented.');
-}
-onCourseUpdated($event: Event) {
-throw new Error('Method not implemented.');
-}
-onToSignalExample() {
-throw new Error('Method not implemented.');
-}
-onToObservableExample() {
-throw new Error('Method not implemented.');
-}
-onAddCourse() {
-throw new Error('Method not implemented.');
-}
+  // Manual Effect Cleanup
+  effectRef: EffectRef | null = null;
 
+  //TODO INMUTABILIDAD - OBJECTOS
+  increment() {
+    this.counter.update((c) => ({...c, value: c.value + 1}));
+  }
+
+  append() {
+    this.values.update(value => [...value, value[value.length - 1] + 1]);
+  }
+
+  constructor() {
+    // Rara ves se usará esta construcción
+    /*afterNextRender(()=>{
+
+      effect(() => {
+        console.log('counter value: ',  this.counter().value)
+      },
+        {
+          injector: this.injector
+        });
+    })*/
+    /** allowSignalWrites
+     *  ojo!!! que podrias crear bucles infinitos
+     */
+
+  //   effect(() => {
+  //     console.log('counter value: ',  this.counter().value)
+  //   },
+  // {allowSignalWrites: false}
+  // );
+
+    this.effectRef = effect((onCleanup) => {
+
+      const counter = this.counter();
+      const timeout = setTimeout(() => {
+        console.log(`counter value: ${counter.value}` )
+      },500)
+
+      onCleanup(()=> {
+        console.log('calling clean up')
+        clearTimeout(timeout)
+      })
+
+    });
+
+  }
+
+  cleanUp() {
+
+      this.effectRef?.destroy();
+
+  }
 
 }
